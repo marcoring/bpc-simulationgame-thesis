5412bfdaaaa710b00f15151f3f878172be8dfef0
marco.ring <marco.ring@myposter.de>
2022-02-13 03:10:15 +0100

    final UI for AppDevAndMaintenance

--- src/views/AppDevAndMaintenance.vue
+++ src/views/AppDevAndMaintenance.vue
@@ -19,31 +19,65 @@
       <!-- Statistic about current, previous round and cost accounting -->
       <v-row>
         <v-col>
-          <!-- Previous Round Status -->
-          <v-card max-height="100%">
-            <v-card-title :style="'background-color:' + teamColor +'!important'" style="color: white">
-              Previous Round
-            </v-card-title>
-            <v-card-text class="mt-3">
-              <p>Outsourcing company: {{ prevOutsComp }}</p>
-              <p>Development cost (EUR): {{ prevDevCost }}</p>
-              <p>Application quality (%): {{ prevAppQual }}</p>
-            </v-card-text>
-          </v-card>
+         <!-- Previous Round Status -->
+        <v-card style="height:100%">
+          <v-card-title :style="'background-color:' + teamColor +'!important'" style="color: white">
+            Previous Round
+          </v-card-title>
+          <v-card-text>
+             <v-text-field
+            label="Outsourcing Company: Development Cost (EUR):"
+            :value="lastVendor != null ? lastVendor.Vendorname : 'No Data'"
+            disabled
+            />
+            <v-text-field
+            label="Sustainability Factor (%):"
+            :value="lastCalculatedSustainabilityfactor != null ? lastCalculatedSustainabilityfactor : 'No Data'"
+            disabled
+            />
+            <v-text-field
+            label="Regionality Factor (%):"
+            :value="lastCalculatedRegionalityfactor != null ? lastCalculatedRegionalityfactor : 'No Data'"
+            disabled
+            />
+            <v-text-field
+            label="Application Quality (%):"
+            :value="lastCalculatedRegionalityfactor != null ? lastCalculatedRegionalityfactor : 'No Data'"
+            disabled
+            />
+          </v-card-text>
+        </v-card>
         </v-col>
 
         <v-col>
-          <!-- Current Round Status -->
-          <v-card max-height="100%">
-            <v-card-title :style="'background-color:' + teamColor +'!important'" style="color: white">
-              Current Round
-            </v-card-title>
-            <v-card-text class="mt-3">
-              <p>Outsourcing company: {{ curOutsComp }}</p>
-              <p>Development cost (EUR): {{ curDevCost }}</p>
-              <p>Application quality (%): {{ curAppQual }}</p>
-            </v-card-text>
-          </v-card>
+        <!-- Current Round Status -->
+        <v-card style="height:100%">
+          <v-card-title :style="'background-color:' + teamColor +'!important'" style="color: white">
+            Current Round
+          </v-card-title>
+          <v-card-text>
+             <v-text-field
+            label="Outsourcing Company: Development Cost (EUR):"
+            :value="lastVendor != null ? lastVendor.Vendorname : 'No Data'"
+            disabled
+            />
+            <v-text-field
+            label="Sustainability Factor (%):"
+            :value="lastCalculatedSustainabilityfactor != null ? lastCalculatedSustainabilityfactor : 'No Data'"
+            disabled
+            />
+            <v-text-field
+            label="Regionality Factor (%):"
+            :value="lastCalculatedRegionalityfactor != null ? lastCalculatedRegionalityfactor : 'No Data'"
+            disabled
+            />
+            <v-text-field
+            label="Application Quality (%):"
+            :value="lastCalculatedRegionalityfactor != null ? lastCalculatedRegionalityfactor : 'No Data'"
+            disabled
+            />
+          </v-card-text>
+        </v-card>
         </v-col>
         </v-row>
 
@@ -68,15 +102,41 @@
       <!-- Managing Application development and maintenance process -->
       <v-row style="margin-top: 10px;">
         <h2 style="text-align: left;">
-          Manage application development and maintenance process
+          Manage Application Development Process
         </h2>
+      
+      <!-- Hover me -->
+      <v-container>
+        <v-col align="start" >
+        <v-tooltip bottom color="black">
+        <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          :color="teamColor"
+          dark
+          v-bind="attrs"
+          v-on="on"
+          x-large
+        >
+        <v-icon>mdi-chat-question </v-icon>
+         Hover me
+        </v-btn>
+        </template>
+        <span>Outsorcing Companies: They differ in price and quality.</span><br>
+        <span>Sustainability factor: Level of sustainability of the company.</span><br>
+        <span>Regionality factor: Level of regionality of the company.</span><br>
+        <span>Quality: Quality of the developed application.</span><br>
+        </v-tooltip>
+        </v-col>
+      </v-container>
+
+
       </v-row>
       <v-row>
         <v-col>
           <v-select
             v-model="selectedCompany"
             :items="outsourcCompany"
-            label="Choose assembly line..."
+            label="Choose outsourcing company ..."
             :color="teamColor"
             item-text="name"
           />
@@ -84,14 +144,32 @@
 
         <v-col>
           <v-text-field
-            label="Application costs (EUR)"
+            label="Outsourcing Company"
             v-model="selectedCompany[0]"
             filled
             disabled
           />
           <v-text-field
-            label="Application quality (%)"
+            label="Sustainability Factor (%)"
             v-model="selectedCompany[1]"
+            filled
+            disabled
+          />
+          <v-text-field
+            label="Regionality Factor (%)"
+            v-model="selectedCompany[2]"
+            filled
+            disabled
+          />
+          <v-text-field
+            label="Quality (%)"
+            v-model="selectedCompany[3]"
+            filled
+            disabled
+          />
+          <v-text-field
+            label="Application Costs (EUR)"
+            v-model="selectedCompany[4]"
             filled
             disabled
           />
